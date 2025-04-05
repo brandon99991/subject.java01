b71c29ec0c139d90680d0c44bea0aff79a58bc46
note15 <test@test.com>
2025-04-05 13:59:30 +0900

    aaa

---
+++ study/IfTest01.java
@@ -0,0 +1,12 @@
+public class IfTest01 {
+    public static void main(String[] args) {
+        int number = 10;
+
+        // If statement
+        if (number == 5) {
+            System.out.println("number의 값은 5와 같다.");
+        } else {
+            System.out.println("number의 값은 5와 같지 않다.");
+        }
+    }
+}
---
+++ study/IfTest02.java
@@ -0,0 +1,14 @@
+public class IfTest02 {
+    public static void main(String[] args) {
+        int number = 10;
+
+        // If statement
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
