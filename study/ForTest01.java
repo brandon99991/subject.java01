591faa50dff284f042358cb92f208b6a6f7882d0
note15 <test@test.com>
2025-04-05 13:44:07 +0900

    add

--- study/ForTest01.java
+++ study/ForTest01.java
@@ -1,7 +1,7 @@
 public class ForTest01 {
 	public static void main(String[] args) {
 		int i;
-		for (i = 0; i <= 10; i++) { // 1~10까지반복
+		for (i = 0; i <= 10; i++) {
 			System.out.print("+");
 		}
 	}
--- study/ForTest03.java
+++ study/ForTest03.java
@@ -1,12 +1,12 @@
 public class ForTest03 {
-	public static void main(String[] args) {
-		int i = 0;
-		int k = 0;
-
-		for (i = 0; i <= 10; i++) { // 1~10까지반복
-			for (k = 0; k <= 10; k++) { // 1~10까지반복
-				System.out.print("+");
-			}
-		}
-	}
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("=======================");
+        	System.out.println("*** i변수의 값은: " + i);
+            
+            for (int j = 1; j <= 3; j++) {
+                System.out.println("j변수의 값은: " + j);
+            }
+        }
+    }
 }
