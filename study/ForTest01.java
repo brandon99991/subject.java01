580a62ab2b431be23cbf366732c6ad3970cc8459
note15 <test@test.com>
2025-04-05 13:35:38 +0900

    aaa

--- study/ForTest01.java
+++ study/ForTest01.java
@@ -1,7 +1,7 @@
 public class ForTest01 {
 	public static void main(String[] args) {
 		int i = 0;
-		for (i = 1; i <= 10; i++) { // 1~10까지반복
+		for (i = 0; i <= 10; i++) { // 1~10까지반복
 			System.out.print("+");
 		}
 	}
--- study/ForTest03.java
+++ study/ForTest03.java
@@ -1,8 +1,12 @@
 public class ForTest03 {
 	public static void main(String[] args) {
 		int i = 0;
-		for (i = 1; i <= 10; i++) { // 1~10까지반복
-			System.out.print("+");
+		int k = 0;
+
+		for (i = 0; i <= 10; i++) { // 1~10까지반복
+			for (k = 0; k <= 10; k++) { // 1~10까지반복
+				System.out.print("+");
+			}
 		}
 	}
 }
