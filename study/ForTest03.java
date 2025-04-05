1b2308ed1052d0d6fc675d6eacb1e41b139c3044
note15 <test@test.com>
2025-04-05 13:30:17 +0900

    aaa

--- study/ForTest03.java
+++ study/ForTest03.java
@@ -1,4 +1,4 @@
-public class ForTest01 {
+public class ForTest03 {
 	public static void main(String[] args) {
 		int sum = 0;
 		for (int i = 1; i <= 10; i++) { // 1~10까지반복
---
+++ study/ForTest1.java
@@ -0,0 +1,8 @@
+public class ForTest1 {
+	public static void main(String[] args) {
+		int i = 0;
+		for (i = 1; i <= 10; i++) { // 1~10까지반복
+			System.out.print("+");
+		}
+	}
+}
