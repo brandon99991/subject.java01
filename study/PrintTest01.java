26837aadaab0dbf13b3315c4c054374cf747fddc
note15 <test@test.com>
2025-04-05 12:48:26 +0900

    add

---
+++ study/PrintTest01.java
@@ -0,0 +1,13 @@
+public class PrintTest01 {
+    public static void main(String[] args) {
+        
+    	// This is println 
+        System.out.println("Hello ");
+        System.out.println("World ");
+
+    	// This is print 
+    	System.out.print("Hello ");
+    	System.out.print("World");
+        
+    }
+}
