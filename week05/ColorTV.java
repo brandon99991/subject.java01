0400f1f53184e1f637315d17e936cedede1a0b04
note15 <test@test.com>
2025-04-12 19:09:03 +0900

    111

--- week05/ColorTV.java
+++ week05/ColorTV.java
@@ -1,12 +1,3 @@
-class TV {
-	private int size;
-	protected int aaa=10;
-	public int bbb=20;
-
-	public TV(int size) { this.size = size; }
-	protected int getSize() { return size; }
-}
-
 public class ColorTV extends TV {
 	private int colors;
 	public ColorTV(int size, int colors) {
@@ -18,10 +9,5 @@ public class ColorTV extends TV {
 		System.out.println(getSize() + "인치 " + colors + "컬러");
 		System.out.println("aaa변수 값 : " + aaa);
 		System.out.println("bbb변수 값 : " + bbb);
-	}
-	
-	public static void main(String[] args) {
-		ColorTV myTv = new ColorTV(65, 65536);
-		myTv.printProperty();		
-	}
+	}	
 }
---
+++ week05/TV.java
@@ -0,0 +1,8 @@
+class TV {
+	private int size;
+	protected int aaa=10;
+	public int bbb=20;
+
+	public TV(int size) { this.size = size; }
+	protected int getSize() { return size; }
+}
